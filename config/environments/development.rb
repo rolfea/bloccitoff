6fb18a118c9cf223f6457368393cd5ab7a10c4b9
Alex Rolfe <rolfepercussion@gmail.com>
2016-01-20 00:30:49 -0600

    Fixed sign-in error in migration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,8 +40,9 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   # Sets default URL options for devise compatibility
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   # Devise email confirmation Settings
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  config.action_mailer.perform_deliveries = true
 end
--- db/migrate/20160120025618_add_confirmation_token_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddConfirmationTokenToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :confirmation_token, :string
-    add_index :users, :confirmation_token
-  end
-end
